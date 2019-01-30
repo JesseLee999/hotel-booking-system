ef48dc98c76f5d7ee2745aec107c30932463afe3
Jesse Dilgee <42963796+JesseLee999@users.noreply.github.com>
2019-01-30 10:40:56 +0200

    initialized project

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.7.2/css/bulma.min.css" />
+    <title>Book</title>
+</head>
+<body>
+    <section class="hero is-fullheight">
+        <div class="hero-body">
+            <div class="container is-fluid">
+            <h1 class="title">Hotel Booking System</h1>
+            </div>
+        </div>
+    </section>
+    
+</body>
+</html>
